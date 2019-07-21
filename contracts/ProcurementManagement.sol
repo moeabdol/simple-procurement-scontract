bb8d524a26d4901027353209c0eef6a1f710d68b
Mohammad Abdullah <mohd.a.saed@gmail.com>
2019-07-21 17:46:53 +0300

    Implement getPurchaseOrdersBySeller method on ProcurementManagement contract

--- contracts/ProcurementManagement.sol
+++ contracts/ProcurementManagement.sol
@@ -53,4 +53,33 @@ contract ProcurementManagement {
 
         return pos;
     }
+
+    function getPurchaseOrdersBySeller(address seller)
+    public view returns (PurchaseOrder[] memory) {
+        uint publicPOCount;
+        uint privatePOCount;
+        for (uint i = 0; i < purchaseOrders.length; i++) {
+            if (keccak256(bytes(purchaseOrders[i].bidType)) == keccak256(bytes("public"))) publicPOCount++;
+            if (keccak256(bytes(purchaseOrders[i].bidType)) == keccak256(bytes("private"))) {
+                for (uint j = 0; j < purchaseOrders[i].sellers.length; j++) {
+                    if (purchaseOrders[i].sellers[j] == seller)
+                        privatePOCount++;
+                }
+            }
+        }
+
+        PurchaseOrder[] memory pos = new PurchaseOrder[](publicPOCount + privatePOCount);
+        uint counter;
+        for (uint i = 0; i < purchaseOrders.length; i++) {
+            if (keccak256(bytes(purchaseOrders[i].bidType)) == keccak256(bytes("public"))) pos[counter++] = purchaseOrders[i];
+            if (keccak256(bytes(purchaseOrders[i].bidType)) == keccak256(bytes("private"))) {
+                for (uint j = 0; j < purchaseOrders[i].sellers.length; j++) {
+                    if (purchaseOrders[i].sellers[j] == seller)
+                        pos[counter++] = purchaseOrders[i];
+                }
+            }
+        }
+
+        return pos;
+    }
 }
